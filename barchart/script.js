57550c599d7f1ae0d8d62a57cd2bf438ae2795f9
Nitish Pal <nitishpal2013@gmail.com>
2023-06-29 00:33:56 +0530

    Plotted the graph successfully.

--- barchart/script.js
+++ barchart/script.js
@@ -1,12 +1,5 @@
 /*
 KEY ACTIONS TO EXECUTE.
-User Story #1: My chart should have a title with a corresponding id="title".
-
-User Story #2: My chart should have a g element x-axis with a corresponding id="x-axis".
-
-User Story #3: My chart should have a g element y-axis with a corresponding id="y-axis".
-
-User Story #4: Both axes should contain multiple tick labels, each with a corresponding class="tick".
 
 User Story #5: My chart should have a rect element for each data point with a corresponding class="bar" displaying the data.
 
@@ -59,19 +52,27 @@ const ymax = d3.max(dataset,(d)=>d[1]);
 const xmin = Number(d3.min(dataset,(d)=>d[0].slice(0,4)));
 const ymin = d3.min(dataset,(d)=>d[1]);
 
-const xScale = d3.scaleLinear().domain([0,xmax]).range([margin, width - margin]);
-const yScale = d3.scaleLinear().domain([0,ymax]).range([height - margin, margin]);
+const xScale = d3.scaleLinear().domain([xmin,xmax]).range([margin, width - margin]);
+const yScale = d3.scaleLinear().domain([0,ymax]).range([height - margin,margin]);
+const anotherScale = d3.scaleLinear().domain([0,dataset.length]).range([margin, width - margin]);
 
 // Make the axis
 const xAxis = d3.axisBottom(xScale);
 const yAxis = d3.axisLeft(yScale);
-
-console.log(xScale(0))
-
 const svg = d3.select("div.container")
 .append("svg")
   .attr("width", width)
   .attr("height", height);
 
-svg.append('g').attr("transform",`translate(0,${height - margin})`).call(xAxis);
-svg.append('g').attr("transform",`translate(${margin},0)`).call(yAxis);
+svg.append('g').attr("id","x-axis").attr("transform",`translate(0,${height - margin})`).call(xAxis);
+svg.append('g').attr("id","y-axis").attr("transform",`translate(${margin},0)`).call(yAxis);
+
+const gap = (width-margin)/dataset.length;
+// axis name
+svg.append("text").attr('class', 'heading').text('Gross Domestic Product');
+
+svg.selectAll('rect').data(dataset).enter().append("rect").attr("class","bar").attr('x',(d,i)=>anotherScale(i)).attr('y',(d,i)=>yScale(d[1]))
+.attr('width',gap).attr('height',(d)=> height - margin - yScale(d[1]))
+.attr("data-date",(d)=>`${d[0]}`).attr("data-gdp",(d)=>`${d[1]}`);
+
+d3.select("div.container").append("div");
