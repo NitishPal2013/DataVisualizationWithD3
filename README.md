41658fa374e622dc72c920f00fa9dcb5c7e7a886
Nitish Pal <nitishpal2013@gmail.com>
2023-07-09 15:24:15 +0530

    Finalise

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Use of D3.js library to represent some data graphically and much more.
 Barchart Link https://nitishpal2013.github.io/DataVisualizationWithD3/barchart/index.html
 
 Barchart https://d3barchart.onrender.com/
+
+ScatterPlot https://nitishpal2013.github.io/DataVisualizationWithD3/scatterplot/index.html
--- scatterplot/script.js
+++ scatterplot/script.js
@@ -1,47 +1,14 @@
-/**
-User Story #1: I can see a title element that has a corresponding id="title".
-
-User Story #2: I can see an x-axis that has a corresponding id="x-axis".
-
-User Story #3: I can see a y-axis that has a corresponding id="y-axis".
-
-User Story #4: I can see dots, that each have a class of dot, which represent the data being plotted.
-
-User Story #5: Each dot should have the properties data-xvalue and data-yvalue containing their corresponding x and y values.
-
-User Story #6: The data-xvalue and data-yvalue of each dot should be within the range of the actual data and in the correct data format.
-For data-xvalue, integers (full years) or Date objects are acceptable for test evaluation. For data-yvalue (minutes), use Date objects.
-
-User Story #7: The data-xvalue and its corresponding dot should align with the corresponding point/value on the x-axis.
-
-User Story #8: The data-yvalue and its corresponding dot should align with the corresponding point/value on the y-axis.
-
-User Story #9: I can see multiple tick labels on the y-axis with %M:%S time format.
-
-User Story #10: I can see multiple tick labels on the x-axis that show the year.
-
-User Story #11: I can see that the range of the x-axis labels are within the range of the actual x-axis data.
-
-User Story #12: I can see that the range of the y-axis labels are within the range of the actual y-axis data.
-
-User Story #13: I can see a legend containing descriptive text that has id="legend".
-
-User Story #14: I can mouse over an area and see a tooltip with a corresponding id="tooltip" which displays more information about the area.
-
-User Story #15: My tooltip should have a data-year property that corresponds to the data-xvalue of the active area.
- */
-
-var url =
+const url =
   'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
-  import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-  
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
 var margin = {
-    top: 100,
-    right: 20,
-    bottom: 30,
-    left: 60
-  },
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
+},
   width = 920 - margin.left - margin.right,
   height = 630 - margin.top - margin.bottom;
 
@@ -157,14 +124,14 @@ d3.json(url)
         div
           .html(
             d.Name +
-              ': ' +
-              d.Nationality +
-              '<br/>' +
-              'Year: ' +
-              d.Year +
-              ', Time: ' +
-              timeFormat(d.Time) +
-              (d.Doping ? '<br/><br/>' + d.Doping : '')
+            ': ' +
+            d.Nationality +
+            '<br/>' +
+            'Year: ' +
+            d.Year +
+            ', Time: ' +
+            timeFormat(d.Time) +
+            (d.Doping ? '<br/><br/>' + d.Doping : '')
           )
           .style('left', event.pageX + 'px')
           .style('top', event.pageY - 28 + 'px');
